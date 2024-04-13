11cb6f3bfd5bb4e482f389556b38216bb340940a
David Elahee <david.elahee@gmail.com>
2024-04-13 15:51:09 +0200

    remove mt for sake of simplicty

--- premake5.lua
+++ premake5.lua
@@ -72,28 +72,6 @@ project "helloworld"
 	filter "system:macosx"
 		links { "QuartzCore.framework", "Metal.framework", "Cocoa.framework", "IOKit.framework", "CoreVideo.framework" }
 	setBxCompat()
-
-project "helloworld_mt"
-	kind "ConsoleApp"
-	language "C++"
-	cppdialect "C++14"
-	exceptionhandling "Off"
-	rtti "Off"
-	files "helloworld_mt.cpp"
-	includedirs
-	{
-		path.join(BGFX_DIR, "include"),
-		path.join(BX_DIR, "include"),
-		path.join(GLFW_DIR, "include")
-	}
-	links { "bgfx", "bimg", "bx", "glfw" }
-	filter "system:windows"
-		links { "gdi32", "kernel32", "psapi" }
-	filter "system:linux"
-		links { "dl", "GL", "pthread", "X11" }
-	filter "system:macosx"
-		links { "QuartzCore.framework", "Metal.framework", "Cocoa.framework", "IOKit.framework", "CoreVideo.framework" }
-	setBxCompat()
 	
 project "bgfx"
 	kind "StaticLib"
